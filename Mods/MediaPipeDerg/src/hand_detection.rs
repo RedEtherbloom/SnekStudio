1c36f334d4b9b3cbe4b0e8d2b8c1045bf085a5b5
RedEtherbloom <etherbloom@mailbox.org>
2026-03-23 16:16:39 +0100

    feat: implemented nms in test

--- Mods/MediaPipeDerg/src/hand_detection.rs
+++ Mods/MediaPipeDerg/src/hand_detection.rs
@@ -1,4 +1,38 @@
+use burn::config::Config;
+
 pub const HAND_DETECTOR_SIZE: usize = 192;
+
+#[derive(Config, Debug)]
+pub struct DetectionConfig {
+    #[config(default = 0)]
+    offset_box: usize, // Offset of box data in row. Usually starts at beginning of row.
+    #[config(default = 4)]
+    number_values_per_box: usize,
+
+    offset_keypoints: usize, // Offset to keypoint data in tensor
+    number_keypoints: usize,
+    number_values_per_keypoint: usize, // Number of predicted values per keypoint
+
+    #[config(default = 0.5)]
+    nms_iou_min_threshold: f32, // Minimum overlap ratio for boxes to be considered duplicate
+    #[config(default = 0.5)]
+    nms_score_min_threshold: f32, // Minimum confidence for detected bounding box to be considered
+    // valid
+
+    // Not yet implemented options
+    #[config(default = 0)]
+    number_rows: usize, // Number of rows to process, or 0 for all
+    #[config(default = 1)]
+    number_classes: usize, // Unused for now
+    #[config(default = 0)]
+    summed_row_size: usize, // Total summed size of a single detection tensor row. Mostly useful for validation.
+    #[config(default = false)]
+    sigmoid_score: bool, // Apply sigmoid function to scores. Used by upstream Mediapipe
+}
+
+pub fn default_hand_detection_config() -> DetectionConfig {
+    DetectionConfig::new(4, 2, 7)
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,12 +40,16 @@ mod tests {
         Tensor,
         backend::NdArray,
         prelude::Backend,
-        tensor::{Shape, TensorData},
+        tensor::{Shape, TensorData, s},
     };
+    use burn_vision::{Nms, NmsOptions};
     use image::{ImageReader, Pixel};
     use tracing::{debug, debug_span, info};
 
-    use crate::{hand_detection::HAND_DETECTOR_SIZE, models::hand_detector::Model};
+    use crate::{
+        hand_detection::{HAND_DETECTOR_SIZE, default_hand_detection_config},
+        models::hand_detector::Model,
+    };
 
     #[test_log::test]
     fn hand_detector_test() {
@@ -64,12 +102,28 @@ mod tests {
         assert!(min >= 0.0);
         debug!("Finished data sanity check");
 
-        let (result1, result2) = model.forward(tensor);
+        let (box_tensor, score_tensor) = model.forward(tensor);
+        debug!(?box_tensor, ?score_tensor, "Finished inference");
+
+        debug!("Beginning Non-Maximum Suppression");
+        let detection_config = default_hand_detection_config();
+        let boxes_subslice = box_tensor.clone().squeeze().slice(s![
+            ..,
+            detection_config.offset_box..detection_config.number_values_per_box,
+        ]);
+        let nms_options = NmsOptions {
+            iou_threshold: detection_config.nms_iou_min_threshold,
+            score_threshold: detection_config.nms_score_min_threshold,
+            max_output_boxes: 0,
+        };
+        let box_indices = boxes_subslice.nms(score_tensor.clone().squeeze(), nms_options);
+        let remaining_boxes = box_tensor.select(1, box_indices.clone());
         debug!(
-            result_tensor_1 = ?result1,
-            result_tensor_2 = ?result2,
-            "Finished inference",
+            ?box_indices,
+            ?remaining_boxes,
+            "Finished Non-Maxium Suppression"
         );
+
         info!("Test succeeded without issues");
     }
 }
